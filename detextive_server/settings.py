e9595e0b56358fb6e0311b7d601f43532705e683
Kristina Solovyova <kris.solo15@gmail.com>
2019-05-17 02:01:16 +0300

    Fix CORS whitelist

--- detextive_server/settings.py
+++ detextive_server/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
     '127.0.0.1:8080',
-    'https://detextive.herokuapp.com'
+    'detextive.herokuapp.com'
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
